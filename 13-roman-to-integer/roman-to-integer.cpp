3c84c16f360e8f152c286127a27d90915c82137f
SAMEER KUMAR SINGH <hss6387331677@gmail.com>
2026-07-18 23:34:24 +0530

    Time: 1 ms (81.12%) | Memory: 13.7 MB (6.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,7 +4,7 @@ public:
      
           unordered_map<char,int> roman={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
    int res=0;
-   for(int i=0;i<s.size() - 1; i++) {
+   for(int i=0;i<=s.size() - 1; i++) {
             if (roman[s[i]] < roman[s[i + 1]]) {
                 res -= roman[s[i]];
             } else {
@@ -12,7 +12,7 @@ public:
             }
         }
 
-        return res + roman[s[s.size() - 1]];
+        return res;
    
     }
 
