f6269e5aa8cbf9ac684670cef11f568053bdfe08
SAMEER KUMAR SINGH <hss6387331677@gmail.com>
2026-07-19 22:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.14%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
       
-       if (n <= 0) {
+       if(n<=0){
             return false;
         }
-        while (n % 2 == 0) {
-            n /= 2;
+        while(n%2==0) {
+            n/=2;
         }
-        return n == 1;
+        if(n==1){
+            return true;
+        }
+        return false;
     }
+
 };
