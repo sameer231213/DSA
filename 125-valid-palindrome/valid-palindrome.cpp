97c5bb935fece70a0162d546c466f76b2571ed16
SAMEER KUMAR SINGH <hss6387331677@gmail.com>
2026-08-01 11:57:14 +0530

    Time: 2 ms (35.87%) | Memory: 10.6 MB (8.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,10 +3,13 @@ public:
     bool isPalindrome(string s) {
          string ans = "";
         for(int i = 0; i < s.size(); i++) {
-            if(isalnum(s[i])) {
-                ans += tolower(s[i]);
+            if(isalnum(s[i])){
+                ans += s[i];
             }
         }
+       for(int i = 0; i < ans.size(); i++) {
+          ans[i]=tolower(ans[i]);
+}
      string rev="";
      for(int i=ans.size()-1;i>=0;i--){
         rev.push_back(ans[i]);
