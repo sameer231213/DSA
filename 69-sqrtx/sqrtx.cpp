9914fbfefd4b1973736c2c7870940bb41caec066
SAMEER KUMAR SINGH <hss6387331677@gmail.com>
2026-08-01 15:56:36 +0530

    Time: 11 ms (12.50%) | Memory: 8.6 MB (50.95%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x==1) return 1;
-                
         int ans=0;
         for(long long i=1;i<x;i++){
             if(i*i<=x)
-               ans=i;
-               else
+                ans=i;
+                else
                 break;
-               
-            
+                
         }
         return ans;
 
