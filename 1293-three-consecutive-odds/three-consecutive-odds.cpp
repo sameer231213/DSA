766484f7cc72ec3c09d93a0d2f8901da86ab6d70
SAMEER KUMAR SINGH <hss6387331677@gmail.com>
2026-08-01 12:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (59.44%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -3,8 +3,8 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
        
         int n=arr.size();
-        for(int i=2;i<n;i++){
-            if(arr[i]%2!=0 && arr[i-1]%2!=0 && arr[i-2]%2!=0){
+        for(int i=0;i<=n-3;i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
                return true;
                break;
             }
